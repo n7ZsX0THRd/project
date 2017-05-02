1d7d5389e2e02a3999d037fc48c4f29c5eba71b5
basvds <bas@easyply.nl>
2017-05-02 14:02:51 +0200

    Login

--- login.php
+++ login.php
@@ -40,11 +40,15 @@
         <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 loginscherm">
           <h1>Inloggen</h1>
           <p>Welkom op de beste veilingsite van Nederland</p>
-            
+
             <div>
             <form class="form-horizontal">
-              
+
+
+
             <div class="login">
+              <p class="bg-warning">Gebruiker niet gevonden</p>
+              <p class="bg-danger">Informatie onjuist</p>
                   <div class="input-group">
                       <div class="input-group-addon"><span class="glyphicon glyphicon-envelope" aria-hidden="true" background="#f0f0f0"></span></div>
                       <input type="email" class="form-control" id="inputEmail" placeholder="Email">
@@ -52,28 +56,31 @@
                   <div class="input-group">
                       <div class="input-group-addon"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></div>
                       <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-                  </div>    
+                  </div>
             </div>
 
               <div class="bevestig">
                 <div class="row">
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <input type="checkbox" class="padding-top"> Remember me
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="position:relative;">
+                        <label for="remember"  class="padding-top"><input id="remember" name="remember" type="checkbox"> Remember me</label>
                     </div>
                     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <button type="submit" class="btn btn-orange align-right" >Sign in</button>  
+                        <button type="submit" class="btn btn-orange align-right" >Sign in</button>
                     </div>
                 </div>
-              </div>  
-                
+              </div>
+              <div class="row">
+                <div class="col-lg-12">
+                  <p class="sub-text-register">Nog geen account? <a href="registreer.php">Registreer dan hier</a></p>
+                </div>
+              </div>
             </form>
             </div>
-            registreer
             </div>
       </div>
-        
+
     </div>
-   
+
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
