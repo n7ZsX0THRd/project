92e2e767c4f8bc26d064afcd62e753d4886fbbd5
otisvdm <otisvdm@hotmail.com>
2017-05-08 14:42:02 +0200

    jhf

--- beheer/ajax.php
+++ beheer/ajax.php
@@ -3,6 +3,11 @@
   pdo_connect();
 
   if($_POST['action'][0] == 'delete') {
-    echo delete_user($_POST['action'][1],$db);
+    if((delete_user($_POST['action'][1],$db))==1) {
+      echo 'Gebruiker verwijderd';
+    }
+    else {
+      echo 'Gebruiker NIET verwijderd';
+    }
   }
 ?>
