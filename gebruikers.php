0769202f2e49c26a66583302df058f39cb8fb9ad
muisje <mmuijsert@outlook.com>
2017-05-09 16:37:05 +0200

    interactief

--- gebruikers.php
+++ gebruikers.php
@@ -417,5 +417,36 @@
             </article>
         </section>
     </main>
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="bootstrap/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script>
+    /*
+    $("li.toggle-sub").click(function(evt) {
+
+      evt.preventDefault();
+      $(this).children("span").toggleClass('glyphicon-menu-right');
+      $(this).children("span").toggleClass('glyphicon-menu-down');
+      $(this).children(".sub").toggle();
+    });
+    */
+    </script>
+    <script type='text/javascript'>
+
+ $(document).ready(function() { 
+   $('input[name=sorteerOp]').change(function(){
+        $('form').submit();
+   });
+  });
+
+</script>
+
   </body>
 </html>
+
+
