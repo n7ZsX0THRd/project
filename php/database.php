9876e178c6cf41b0a611ae40922bb04f47c36db2
otisvdm <otisvdm@hotmail.com>
2017-05-10 10:36:57 +0200

    voorsam

--- php/database.php
+++ php/database.php
@@ -81,12 +81,19 @@ function update_user($data,$db){
       geboortedatum=?,
       emailadres=?,
       biografie=?,
+
       WHERE gebruikersnaam = ?");
 
       $dbs->execute(array($data['p_firstname'],$data['p_lastname'],$data['p_adres'],
       $data['p_zipcode'],$data['p_city'],$data['p_birthday'].'-'.$data['p_birthmonth'].'-'.$data['p_birthyear'],
       $data['p_email'],$data['p_biografie'],$data['p_username']));
 
+      $dbs = $db->prepare(" UPDATE Gebruikerstelefoon SET
+      p_tel =?,
+
+      WHERE gebruikersnaam = ?"
+      $dbs->execute(array($data['p_tel'],$data['p_username']));
+      
       return true;
   } catch (PDOException $e) {
       return $e;
