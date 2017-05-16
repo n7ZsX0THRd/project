e55152c99b514d563fb90300ff9db8274b50a884
muisje <mmuijsert@outlook.com>
2017-05-16 14:24:33 +0200

    Hash secret vraag

--- php/database.php
+++ php/database.php
@@ -95,19 +95,19 @@ function create_user($data,$db){ //db is global!!
       VALUES (?,?,?,?,?,?,?,?,?,?,?); INSERT INTO Gebruikerstelefoon (gebruikersnaam,telefoonnummer) VALUES (?,?)");
 
       $dbs->execute(array(
-          $data['r_username'],
-          $data['r_firstname'],
-          $data['r_lastname'],
-          $data['r_street_name'].' '.$data['r_street_nr'].' '.$data['r_street_addition'],
-          $data['r_zipcode'],
-          $data['r_city'],
-          $data['r_birthmonth'].'-'.$data['r_birthday'].'-'.$data['r_birthyear'],
-          $data['r_email'],
-          password_hash($data['r_password'], PASSWORD_DEFAULT),
-          $data['r_secret_question'],
-          $data['r_secret_question_answer'],
-          $data['r_username'],
-          $data['r_phonenumber']
+        $data['r_username'],
+        $data['r_firstname'],
+        $data['r_lastname'],
+        $data['r_street_name'].' '.$data['r_street_nr'].' '.$data['r_street_addition'],
+        $data['r_zipcode'],
+        $data['r_city'],
+        $data['r_birthmonth'].'-'.$data['r_birthday'].'-'.$data['r_birthyear'],
+        $data['r_email'],
+        password_hash($data['r_password'], PASSWORD_DEFAULT),
+        $data['r_secret_question'],
+        password_hash($data['r_secret_question_answer'], PASSWORD_DEFAULT),
+        $data['r_username'],
+        $data['r_phonenumber']
         )
       );
       return true;
