9967900d0fc9fdefe319317764d37b779acb9ac1
Samvanboort <samvanboort@hotmail.com>
2017-05-22 13:05:35 +0200

    Revert "Fix 3.0"
    This reverts commit ba5dcac1f86656392e2a36e4cccbbb07f2cbbca6.

--- login.php
+++ login.php
@@ -27,17 +27,12 @@ if($_POST['form_name']=='requestanswer'){
             return $randstring;
         }
           $randomkey = RandomString();
-
-          $username =$db->prepare("SELECT gebruikersnaam FROM Gebruikers WHERE emailadres=?");
-          $username->execute(array($_POST['emailww']));
-          $usernamenaam = $username->fetchAll()[0];
-
           $nieuweww = $db->prepare("UPDATE Gebruikers SET wachtwoord=? WHERE emailadres=?");
           $nieuweww->execute(array(password_hash($randomkey, PASSWORD_DEFAULT), $_POST['emailww']));
           sendMail($_POST['emailww'],'Nieuw wachtwoord','<table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: '.'Varela Round'.', sans-serif;">
               <tr>
                   <td style="color:#023042">
-                      Beste '.$usernamenaam['gebruikersnaam'].'
+                      Beste Username,
                   </td>
               </tr>
               <tr>
@@ -47,7 +42,7 @@ if($_POST['form_name']=='requestanswer'){
               </tr>
               <tr>
                   <td style="padding: 20px 0 0 0; color:#023042">
-                      <p>Dit is uw nieuwe wachtoord '.$randomkey.'</p>
+                      <p>Dit is uw nieuwe wachtoord '.$randomkey.';</p>
                   </td>
               </tr>
               <tr>
