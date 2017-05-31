864f9dd8ec07dc529c83dc5c6ffdd08e64c5dc80
basvds <bas@easyply.nl>
2017-05-31 11:34:41 +0200

    Verwijder overbodige paginas

--- default_templates/index.php
+++
@@ -1,8 +0,0 @@
-<h1>Standaard Templates</h1>
-<?php
-$files = glob('*.{html,php,js,css}', GLOB_BRACE);
-foreach($files as $file) {
-  if($file !== 'index.php')
-  echo '<li><a href="'.$file.'">'.$file.'</a></li>';
-}
-?>
