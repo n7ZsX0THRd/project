06bb84cff29b59899e1d8eeb00648a12ce2a4d8a
muisje <mmuijsert@outlook.com>
2017-06-06 13:37:23 +0200

    deels

--- verkoper_worden.php
+++ verkoper_worden.php
@@ -17,9 +17,32 @@ pdo_connect();
 
 $error = 0;
 
+
 $page = '';
 
- if(isUserLoggedIn($db))
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  if(isset($_POST["controle"]) && !empty($_POST["controle"])){
+        if ($_POST["controle"]=="creditcard"){
+          $page='bevestigd';
+        } else if ($_POST["controle"]=="post"){
+          $page='verwerking';
+        }
+  } else {
+
+    if(isset($_POST['page']) && !empty($_POST['page'])){
+      $page=$_POST['page'];
+    }
+
+  }
+
+}
+
+$page = 'bevestigd';
+
+
+
+ if(!isUserLoggedIn($db))
    header('location: index.php');
   // If the user is logged In, redirect to homepage.
   // No purpose to get on this page
@@ -47,7 +70,7 @@ $page = '';
 ?>
 <main class="container">
 <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 loginscherm">
-<form class="form-horizontal" method="post" name="word_verkoper" enctype="multipart/form-data" action="">
+<div class="form-horizontal" >
   <h2>Verkoper worden</h2>
   <?php 
   switch ($page) {
@@ -55,29 +78,37 @@ $page = '';
     ?>
           <!-- login gegevens -->
           <div class="login">
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon glyphicon-piggy-bank" aria-hidden="true" background="#f0f0f0"></span></div>
-                <input type="bank" class="form-control" id="bank" name="bank" placeholder="Bank">
+            <form action="activatie.php" method="POST">
+              <div class="input-group">
+                <div class="input-group-addon"><span class="glyphicon glyphicon glyphicon-piggy-bank" aria-hidden="true" background="#f0f0f0"></span></div>
+                  <input type="text" pattern="^[A-Za-z_ ]{1,15}$" class="form-control" id="bank" name="bank" placeholder="Bank"  required>
+              </div>
+              <br>
+              <div class="input-group">
+                <div class="input-group-addon"><span class="glyphicon glyphicon glyphicon glyphicon-credit-card" aria-hidden="true"></span></div>
+                  <input type="text" pattern="[0-9]{13,16}" class="form-control" id="creditcardnummer" placeholder="Creditcard nummer" name="creditcardnummer"  required>
+                  <!-- test with http://www.getcreditcardnumbers.com/ -->
+                  <br>
+              </div>
+              <br>
             </div>
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon glyphicon glyphicon-credit-card" aria-hidden="true"></span></div>
-                <input type="rekeningnummer" class="form-control" id="rekeningnummer" placeholder="Rekeningnummer" name="rekeningnummer" value="">
-            </div>
-          </div>
 
 
           <div class="row">
             <label class="col-lg-12">Controle optie</label>
-              <input class="col-lg-4 col-md-4 col-sm-4" type="radio" name="controle" value="post"> Post<br>
+              <input class="col-lg-4 col-md-4 col-sm-4" type="radio" name="controle" value="post" required> Post<br>
               <input class="col-lg-4 col-md-4 col-sm-4" type="radio" name="controle" value="creditcard"> Creditcard<br>
           </div>
 
-          <div class="bevestig">
-            <div class="row">
-              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <button type="submit" class="btn btn-orange align-right">Word verkoper</button>
+            <div class="bevestig">
+              <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                  <button type="submit" value="Submit" class="btn btn-orange align-right">Word verkoper</button>
+                  <br>
+                </div>
               </div>
-            </div>
+              <br>
+            </form>
           </div>
         <?php
         break;
@@ -86,16 +117,37 @@ $page = '';
           <p>Vul hier de code in die je via post hebt ontvangen</p>
           <div class="input-group">
               <div class="input-group-addon"><span class="glyphicon glyphicon glyphicon-lock" aria-hidden="true"></span></div>
-                <input type="code" class="form-control" id="code" placeholder="Code" name="code" value="">
+                <input type="text" pattern="[0-9]{6}" class="form-control" id="code" placeholder="Code" name="code" value="">
             </div>
 
           <div class="bevestig">
             <div class="row">
               <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <button type="submit" class="btn btn-orange align-right">Bevestig code</button>
+                
               </div>
             </div>
+            <br>
           </div>
+          <?php
+          // activatie code invoeren
+        break;
+        case 'bevestigd':
+          ?>
+          <h3>Succes!</h3>
+          <p>Uw account is geregistreerd als verkoper account! <br> 
+          <a href="veilingtoevoegen.php">Begin een veiling</a>
+          <br></p>
+ 
+          <?php
+          // activatie code invoeren
+        break;
+        case 'verwerking':
+          ?>
+          <h3>Succes!</h3>
+          <p>Uw aanvraag is ontvangen, de gegevens worden gecontroleerd. binnenkort ontvangt u een brief met instructies om het account te activeren.<br> 
+          <br></p>
+ 
           <?php
           // activatie code invoeren
         break;
@@ -107,7 +159,7 @@ $page = '';
               <div class="row">
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                   <form action="" method="POST">
-                    <input type="hidden" name="keuze" value="registreren">
+                    <input type="hidden" name="page" value="registreren">
                     <button type="submit" class="btn btn-niagara">Verkoper worden</button>
                   </form>
                 </div>
@@ -116,7 +168,7 @@ $page = '';
               <div class="row">
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                   <form action="" method="POST">
-                    <input type="hidden" name="keuze" value="activeren">
+                    <input type="hidden" name="page" value="activeren">
                     <button type="submit" class="btn btn-orange">Mijn code controleren</button>
                   </form>
                 </div>
@@ -127,7 +179,7 @@ $page = '';
           }
         ?>
 
-        </form>
+        </div>
         </div>
         </main>
 <?php include 'php/includes/footer.php' ?>
