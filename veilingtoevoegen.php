3b087a8c507255cf3a78527a14767ce4cc94c0bd
basvds <bas@easyply.nl>
2017-06-07 15:33:56 +0200

    saf

--- veilingtoevoegen.php
+++ veilingtoevoegen.php
@@ -166,7 +166,7 @@ $queryCountries = $db->query("SELECT lnd_Code,lnd_Landnaam FROM Landen");
                       <div class="col-lg-12">
                         <div class="form-group">
                           <label for="vt_description">Beschrijving</label>
-                          <textarea class="form-control" rows="10" id="vt_description" name="vt_description" style="max-width:100%;" placeholder="Beschrijving (verplicht)" maxlength="1024" ></textarea>
+                          <textarea class="form-control" rows="10" id="vt_description" name="vt_description" style="max-width:100%;" placeholder="Beschrijving (verplicht)"></textarea>
                         </div>
                         <!--
                           1,3,5,7,10
